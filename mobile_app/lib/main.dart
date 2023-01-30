791d5475d03d3b4d96646a4806daf4b037a4f3bb
sherif <sherifahmedelsayed2@gmail.com>
2023-01-30 15:18:49 +0200

    add the model

--- mobile_app/lib/main.dart
+++ mobile_app/lib/main.dart
@@ -1,10 +1,10 @@
-import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:mobile_app/screens/about_screen.dart';
 import 'package:mobile_app/screens/history_screen.dart';
 import 'package:mobile_app/screens/home_screen.dart';
 import 'package:mobile_app/screens/info_screen.dart';
+import 'package:mobile_app/screens/splash_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -27,41 +27,6 @@ class MyApp extends StatelessWidget {
         HistoryScreen.id: (context) => const HistoryScreen()
       },
       theme: ThemeData(fontFamily: "CodeNewRoman"),
-    );
-  }
-}
-
-class SplashScreen extends StatefulWidget {
-  const SplashScreen({super.key});
-
-  @override
-  State<SplashScreen> createState() => _SplashScreenState();
-}
-
-class _SplashScreenState extends State<SplashScreen> {
-  @override
-  Widget build(BuildContext context) {
-    return AnimatedSplashScreen(
-      backgroundColor: Colors.pink.shade100,
-      splash: Column(
-        children: [
-          Image.asset("assets/ribbon.png", scale: 3),
-          const SizedBox(height: 20),
-          const Text(
-            "Breast Cancer Checker",
-            style: TextStyle(
-              fontSize: 20,
-              fontWeight: FontWeight.bold,
-              color: Colors.pink,
-            ),
-          )
-        ],
-      ),
-      nextScreen: const HomeScreen(),
-      duration: 5000,
-      animationDuration: const Duration(seconds: 1),
-      centered: true,
-      splashIconSize: 300,
     );
   }
 }
