c93bd5b0b08f657bfc9c835a11214bfb18bfb8c2
sherif <sherifahmedelsayed2@gmail.com>
2023-02-03 19:17:01 +0200

    update to flask

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI, File, UploadFile 
+# from fastapi import FastAPI, File, UploadFile 
+from flask import Flask,request
 import tensorflow as tf
 from keras.models import load_model
 import json 
@@ -8,7 +9,9 @@ import cv2
 # from PIL import Image
 # import scipy.ndimage
  
-app = FastAPI() 
+# app = FastAPI()
+app = Flask()
+
 input_img_size = 256
 model = load_model('cnn2breastcancer.hdf5', compile = False)    
 
@@ -16,18 +19,21 @@ def resize_img(img):
     img=cv2.resize(img,(input_img_size,input_img_size))
     return img
 
-@app.post('/')
-async def scoring_endpoint(data: UploadFile = File(...)): 
-    image_bytes = await data.read()
-    image = tf.io.decode_image(image_bytes)
-    image = np.array(image).astype('uint8')
-    image = resize_img(image)
-    image = np.array(image)
-    yhat = model.predict(tf.expand_dims(image, axis=0))
-    predicted_class = np.argmax(yhat)
-    print(image.shape)
-    return {"Tumor Detection": json.dumps(predicted_class.tolist())}
+# @app.post('/')
+# async def scoring_endpoint(data: UploadFile = File(...)): 
+    # image_bytes = await data.read()
+    # image = tf.io.decode_image(image_bytes)
+    # image = np.array(image).astype('uint8')
+    # image = resize_img(image)
+    # image = np.array(image)
+    # yhat = model.predict(tf.expand_dims(image, axis=0))
+    # predicted_class = np.argmax(yhat)
+    # print(image.shape)
+    # return {"Tumor Detection": json.dumps(predicted_class.tolist())}
      
 @app.route("/test")
 def test():
     return "The cloud api is connected"
+
+if __name__ == "__main__":
+    app.run(debug=False)
